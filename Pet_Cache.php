8f08a6cce2450c6cd0df7008e9e280a62fd95444
sergio.martinez <sergio.martinez@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-08 12:45:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tdplugin-es/trunk@191493 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Pet_Cache.php
+++ Pet_Cache.php
@@ -2,8 +2,15 @@
 // Pide un fichero
 
 function request_cache($url, $dest_file, $timeout) {
-	if(!file_exists($dest_file) || filemtime($dest_file) < (time()-$timeout)) {
+	if(!file_exists($dest_file)){
 		$data = file_get_contents($url);
+		if ($data === false) return false;
+		$tmpf = tempnam('/tmp','YWS');
+		$fp = fopen($tmpf,"w");
+		fwrite($fp, $data);
+		fclose($fp);
+		rename($tmpf, $dest_file);
+	} else if  (filemtime($dest_file) < (time()-$timeout)) {		unlink($dest_file);		$data = file_get_contents($url);
 		if ($data === false) return false;
 		$tmpf = tempnam('/tmp','YWS');
 		$fp = fopen($tmpf,"w");
